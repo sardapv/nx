22eb321cf3fc5543ed408810cc6357ecbef54124
Juri <juri.strumpflohner@gmail.com>
2021-06-13 14:23:43 +0200

    cleanup(angular): re-enable disabled storybook tests

--- e2e/angular/src/storybook.test.ts
+++ e2e/angular/src/storybook.test.ts
@@ -197,7 +197,7 @@ describe('Storybook schematics', () => {
       }
     }, 1000000);
 
-    xit('should build an Angular based storybook', () => {
+    it('should build an Angular based storybook', () => {
       const angularStorybookLib = uniq('test-ui-lib');
       createTestUILib(angularStorybookLib);
       runCLI(
@@ -212,7 +212,7 @@ describe('Storybook schematics', () => {
       ).toContain(`<title>Storybook</title>`);
     }, 1000000);
 
-    xit('should build an Angular based storybook that references another lib', () => {
+    it('should build an Angular based storybook that references another lib', () => {
       const angularStorybookLib = uniq('test-ui-lib');
       createTestUILib(angularStorybookLib);
       runCLI(
